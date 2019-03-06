1bf8a7a5882412ce8eabbfb00ff38ebd572b9f4a
jfqd <jfqd@blun.org>
2019-03-06 10:18:05 +0100

    make number of sending-processes an option

--- copy/opt/qutic/var/mdata-setup/includes/40-mailtrain.sh
+++ copy/opt/qutic/var/mdata-setup/includes/40-mailtrain.sh
@@ -13,6 +13,11 @@ if mdata-get mailtrain_mysqldbname 1>/dev/null 2>&1; then
   sed -i "s/database=\"mailtrain-db\"/database=\"${MYSQDB}\"/" /home/node/mailtrain/config/production.toml
 fi
 
+if mdata-get mailtrain_sender_processes 1>/dev/null 2>&1; then
+  SENDER_PROCESSES=`mdata-get mailtrain_sender_processes`
+  sed -i "s/processes=10/processes=${SENDER_PROCESSES}/" /home/node/mailtrain/config/production.toml
+fi
+
 SESSION_SECRET=`dd if=/dev/urandom bs=32 count=1 2>/dev/null | shasum -a 512 | awk '{print $1}' | tr -d '\n'`
 sed -i "s/secret=\"a cat\"/secret=\"${SESSION_SECRET}\"/" /home/node/mailtrain/config/production.toml
 svcadm enable svc:/application/mailtrain:default
