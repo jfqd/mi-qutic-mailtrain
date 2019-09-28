66f1d46bacb2786deaec4b4bdfe7f0dd2618d63d
jfqd <jfqd@blun.org>
2019-09-28 12:47:52 +0200

    fix user

--- copy/opt/qutic/var/mdata-setup/includes/05-node-authorized-keys.sh
+++ copy/opt/qutic/var/mdata-setup/includes/05-node-authorized-keys.sh
@@ -6,8 +6,8 @@ if mdata-get node_authorized_keys 1>/dev/null 2>&1; then
   mkdir -p ${home}/.ssh
   echo "# This file is managed by mdata-get node_authorized_keys" \
     > ${home}/.ssh/authorized_keys
-  mdata-get admin_authorized_keys >> ${home}/.ssh/authorized_keys
+  mdata-get node_authorized_keys >> ${home}/.ssh/authorized_keys
   chmod 0700 ${home}/.ssh
   chmod 0644 ${home}/.ssh/authorized_keys
-  chown ruby:ruby ${home}/.ssh/authorized_keys
+  chown -R node:node ${home}/.ssh
 fi
