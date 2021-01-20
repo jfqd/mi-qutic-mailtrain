2cce484f8a142216bea6087f51b00d44c1f94bab
Stefan Husch <jfqd@blun.org>
2021-01-20 08:50:24 +0100

    add config for mailtrain mysql-host

--- copy/opt/qutic/var/mdata-setup/includes/40-mailtrain.sh
+++ copy/opt/qutic/var/mdata-setup/includes/40-mailtrain.sh
@@ -1,3 +1,8 @@
+if mdata-get mailtrain_mysqlhost 1>/dev/null 2>&1; then
+  MYSQLHOST=`mdata-get mailtrain_mysqlhost`
+  sed -i "s/host=\"127.0.0.1\"/host=\"${MYSQLHOST}\"/" /home/node/mailtrain/config/production.toml
+fi
+
 if mdata-get mailtrain_mysqluser 1>/dev/null 2>&1; then
   MYSQLUSER=`mdata-get mailtrain_mysqluser`
   sed -i "s/user=\"mailtrain-user\"/user=\"${MYSQLUSER}\"/" /home/node/mailtrain/config/production.toml
@@ -21,3 +26,6 @@ fi
 SESSION_SECRET=`dd if=/dev/urandom bs=32 count=1 2>/dev/null | shasum -a 512 | awk '{print $1}' | tr -d '\n'`
 sed -i "s/secret=\"a cat\"/secret=\"${SESSION_SECRET}\"/" /home/node/mailtrain/config/production.toml
 svcadm enable svc:/application/mailtrain:default
+
+
+host="localhost"
